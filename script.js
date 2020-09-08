f3cdb4016dc286cc4cacadb06ce630b380ee08dd
Niklas Furberg <niklas.furberg@gmail.com>
2020-09-08 10:53:43 +0200

    Refactored both mobels into same solution, made model selectable, minor style changes

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const video = document.getElementById('video');
 const title = document.getElementById('title');
+const selectedModel = document.getElementById('selectedModel');
 const score = document.getElementById('score');
+var interval;
 
 Promise
   .all([
@@ -29,13 +31,28 @@ function startMedia() {
     });
 }
 
-video.addEventListener('play', () => {
-  setInterval(async () => {
+function changeModel(){
+    modelName = document.getElementById("model").value;
+    selectedModel.innerHTML = modelName;
+    score.innerHTML = 'null';
+    title.style.opacity = '0';    
+    clearInterval(interval);
+    if(modelName === "Tiny Face") {
+      startDetection(() => new faceapi.TinyFaceDetectorOptions({ scoreThreshold: 0.1 }));
+      return;
+    }
+    if(modelName === "Ssd Mobile Net") {
+      startDetection(() => new faceapi.SsdMobilenetv1Options({ minConfidence: 0.1 }));
+      return;
+    }    
+}
+
+function startDetection(getOptions) {
+  interval = setInterval(async () => {
     const detection = await faceapi
       .detectSingleFace(
         video, 
-        new faceapi
-          .TinyFaceDetectorOptions({ scoreThreshold: 0.1 }));
+        getOptions());
           
     if(detection == null) {
       score.innerHTML = 'null';
@@ -51,5 +68,10 @@ video.addEventListener('play', () => {
     }
     console.log('Detection score: ' + detection._score.toFixed(2));      
     score.innerHTML = detection._score.toFixed(2);
-  }, 500);
-});
+  }, 500);  
+}
+
+video.addEventListener(
+  'play', 
+  () => startDetection(() => new faceapi.TinyFaceDetectorOptions({ scoreThreshold: 0.1 })), 
+  500);
