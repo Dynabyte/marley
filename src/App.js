3036ab58d32f15d479242c10fc2b26c3326b0918
Niklas Furberg <niklas.furberg@gmail.com>
2020-09-17 13:23:29 +0200

    Formated code

--- src/App.js
+++ src/App.js
@@ -5,28 +5,19 @@ import {
 } from "face-api.js";
 import React from "react";
 
-
-
 function App() {
-
   var list = [];
   for (var i = 145; i <= 375; i++) {
     list.push(i);
   }
 
-
   return (
     <div>
-      {list.map((_,index) => 
-        <img src={"images/daniel-vardagsrum1/0" + _ + ".jpg"}/>
-  
-    )};
+      {list.map((_, index) =>
+        <img src={"images/daniel-vardagsrum1/0" + _ + ".jpg"} />
+      )};
     </div>
-   
-    
   );
-  }
-
-
+}
 
 export default App;
