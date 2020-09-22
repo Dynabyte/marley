bd4ec7207c4595e30dff2d496b6de854efbdc8d3
daniel-hughes-nackademin <daniel.hughes.sweden@gmail.com>
2020-09-22 09:57:53 +0200

    updated tests

--- index.js
+++ index.js
@@ -1,58 +1,40 @@
 /*
 Mål:
-Testa alla bilder mot alla andra bilder för att veta hur många frames framåt och bakåt som ger bra resultat, gärna mycket statistik:
-Alla scores (euclydian distance), medeltal av scores, träff/inte träff...
-Hur många gånger får man inte ut ett ansikte alls (inga landmarks)
-
-Testa dels inbördes för varje bild i en film, sen jämför en film med en annan (samma person)
+Få ut singelsnitten, dvs minimum & average euclidean distance per bild i film 1 jämfört med hela film 2 
+Kolla avvikelser från genomsnittet, dvs avvikelse från minimum och från average. Kolla även absoluta avvikelserna, dvs endast positiva värden
 */
 
-const divDaniel1 = document.getElementById("daniel1");
-const divDaniel2 = document.getElementById("daniel2");
-const divNiklas1 = document.getElementById("niklas1");
+const divFilm1 = document.getElementById("film1");
+const divFilm2 = document.getElementById("film2");
 
-const imagesHtmlDaniel1 = [];
-const imagesHtmlDaniel2 = [];
-const imagesHtmlNiklas1 = [];
+const imagesHtmlFilm1 = [];
+const imagesHtmlFilm2 = [];
 
-const faceDetectionsDaniel1 = [];
-const faceDetectionsDaniel2 = [];
-const faceDetectionsNiklas1 = [];
+const faceDetectionsFilm1 = [];
+const faceDetectionsFilm2 = [];
 
 
 
-const imageNumbersDaniel1 = [];
-for (var i = 145; i <= 375; i++) {
-    imageNumbersDaniel1.push(i);
+const imageNumbersFilm1 = [];
+for (var i = 200; i <= 400; i++) {
+    imageNumbersFilm1.push(i);
 }
 
-
-
-imageNumbersDaniel1.forEach( (imageNumber, i) => {
-    divDaniel1.innerHTML += '<img id="daniel1Image' + i + '" src="images/daniel-vardagsrum1/0' + imageNumber + '.jpg"/>';
-    imagesHtmlDaniel1.push(document.getElementById("daniel1Image" + i));
+imageNumbersFilm1.forEach( (imageNumber, i) => {
+    divFilm1.innerHTML += '<img id="film1Image' + i + '" src="images/kontor-kaffe-daniel2/0' + imageNumber + '.jpg"/>';
+    imagesHtmlFilm1.push(document.getElementById("film1Image" + i));
 })
 
-const imageNumbersDaniel2 = [];
-for (var i = 129; i <= 256; i++) {
-    imageNumbersDaniel2.push(i);
+
+
+const imageNumbersFilm2 = [];
+for (var i = 150; i <= 350; i++) {
+    imageNumbersFilm2.push(i);
 }
 
-imageNumbersDaniel2.forEach( (imageNumber, i) => {
-    divDaniel2.innerHTML += '<img id="daniel2Image' + i + '" src="images/daniel-vardagsrum2/0' + imageNumber + '.jpg"/>';
-    imagesHtmlDaniel2.push(document.getElementById("daniel2Image" + i));
-})
-
-const imageNumbersNiklas1 = [];
-for (var i = 125; i <= 284; i++) {
-    imageNumbersNiklas1.push(i);
-}
-
-
-
-imageNumbersNiklas1.forEach( (imageNumber, i) => {
-    divNiklas1.innerHTML += '<img id="niklas1Image' + i + '" src="images/niklas-ingång-vallentuna/0' + imageNumber + '.jpg"/>';
-    imagesHtmlNiklas1.push(document.getElementById("niklas1Image" + i));
+imageNumbersFilm2.forEach( (imageNumber, i) => {
+    divFilm2.innerHTML += '<img id="film2Image' + i + '" src="images/kontor-kaffe-daniel-720p/0' + imageNumber + '.jpg"/>';
+    imagesHtmlFilm2.push(document.getElementById("film2Image" + i));
 })
 
 Promise
@@ -61,17 +43,17 @@ Promise
       faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
       faceapi.nets.faceRecognitionNet.loadFromUri('/models')
     ]).then(() => {
-      loadFaceDetectionArrayAndGetDetectionStatistics("Daniel1", imagesHtmlDaniel1, faceDetectionsDaniel1);
-      loadFaceDetectionArrayAndGetDetectionStatistics("Daniel2", imagesHtmlDaniel2, faceDetectionsDaniel2);
-      loadFaceDetectionArrayAndGetDetectionStatistics("Niklas1", imagesHtmlNiklas1, faceDetectionsNiklas1);
+      loadFaceDetectionArrayAndGetDetectionStatistics("Film1", imagesHtmlFilm1, faceDetectionsFilm1);
+      loadFaceDetectionArrayAndGetDetectionStatistics("Film2", imagesHtmlFilm2, faceDetectionsFilm2);
+
       setTimeout(()=> { //Try to make sure it runs after detectionArrays have loaded
-        console.log(faceDetectionsDaniel1);
-        console.log(faceDetectionsDaniel2);
-        console.log(faceDetectionsNiklas1);
-        compareTwoDetectionArrays(faceDetectionsDaniel1, faceDetectionsDaniel1);
-        //compareTwoDetectionArrays(faceDetectionsDaniel1, faceDetectionsDaniel2);
-        //compareTwoDetectionArrays(faceDetectionsDaniel1, faceDetectionsNiklas1);
-      }, 30000);
+        console.log(faceDetectionsFilm1);
+        console.log(faceDetectionsFilm2);
+        compareTwoDetectionArrays(faceDetectionsFilm1, faceDetectionsFilm1);
+        compareTwoDetectionArrays(faceDetectionsFilm1, faceDetectionsFilm2);
+        compareTwoDetectionArrays(faceDetectionsFilm2, faceDetectionsFilm2);
+        compareTwoDetectionArrays(faceDetectionsFilm2, faceDetectionsFilm1);
+      }, 200000);
     })
     .catch(function (err) {
       console.error(err);
@@ -168,8 +150,6 @@ Average euclidean distance: ${(sum(euclideanDistancesArray)/euclideanDistancesAr
 Number of comparisons: ${euclideanDistancesArray.length}`;
 
       console.log(fullComparisonReport);
-
-      console.log(euclideanDistancesArray);
       
     }
 
