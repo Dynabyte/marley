96bbfe55e53338fc24d7425b76ede40c4f3f5a2f
Kevin Helly <kevin.helly@dynabyte.se>
2020-09-22 13:21:24 +0200

    Fixed some typings

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,44 @@
 import React from 'react';
 import './App.css';
-import { DiffCamEngine, ICapturePayload, IDiffCamEngine } from './diff-cam-engine';
+import { DiffCamEngine } from './diff-cam-engine';
+import { ICapturePayload, IDiffCamEngine } from './models/diffCamEngine.models';
 
 export const App = () => {
-  const [videoElement, setVideoElement] = React.useState<HTMLElement | null>(
-    null
-  );
-  const [canvasElement, setCanvasElement] = React.useState<HTMLElement | null>(
-    null
-  );
-  // const [dataUrl, setDataUrl] = React.useState<string>('');
+  const [dataUrl, setDataUrl] = React.useState<string>('');
 
-  React.useEffect(() => {
-    setVideoElement(document.getElementById('video'));
-    setCanvasElement(document.getElementById('canvas'));
+  const diffCamEngine: IDiffCamEngine = DiffCamEngine();
+  const initSuccess: () => void = () => {
+    diffCamEngine.start();
+  };
 
-    const diffCamEngine: IDiffCamEngine = DiffCamEngine();
-    const initSuccess = () => {
-      diffCamEngine.start();
-    };
+  const initError: (error: any) => void = (error: any) => {
+    console.log(error);
+  };
 
-    const initError = (error: any) => {
-      console.log(error);
-    };
-
-    const capture = (payload: ICapturePayload) => {
-      if (payload.hasMotion) {
-        console.log(payload.hasMotion, payload.getURL());
-      }
-    };
-
-    diffCamEngine.init({
-      video: videoElement,
-      motionCanvas: canvasElement,
-      initSuccessCallback: initSuccess,
-      initErrorCallback: initError,
-      captureCallback: capture,
-      captureIntervalTime: 5000,
-    });
-  }, [videoElement, canvasElement]);
-
+  const capture: (payload: ICapturePayload) => void = (
+    payload: ICapturePayload
+  ) => {
+    if (payload.hasMotion) {
+      setDataUrl(payload.getURL());
+      console.log(payload.hasMotion, payload.getURL());
+    } else {
+      setDataUrl('');
+    }
+  };
+  const videoElement: HTMLVideoElement = document.createElement('video');
+  const canvasElement: HTMLCanvasElement = document.createElement('canvas');
+  diffCamEngine.init({
+    video: videoElement,
+    motionCanvas: canvasElement,
+    initSuccessCallback: initSuccess,
+    initErrorCallback: initError,
+    captureCallback: capture,
+    captureIntervalTime: 2000,
+  });
   return (
     <div className='App'>
-      <span style={{ opacity: 0, position: 'fixed' }}>
-        <canvas id='canvas'></canvas>
-        <video
-          id='video'
-          width='640'
-          height='480'
-          muted={true}
-          autoPlay={true}
-        ></video>
-      </span>
+      <span style={{ opacity: 0, position: 'fixed' }}></span>
+      <img src={dataUrl} alt='Bilden som skickas' />
     </div>
   );
 };
