fab0f637646008ffbf052db640163b8d37f7bf9b
Alexandra Onegård <alexandra.onegard@dynabyte.se>
2020-09-29 10:05:01 +0200

    Fix wrong import, remove strict mode so code doesnt run twice

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
-import React from "react";
-import "./App.css";
-import { DiffCamEngine } from "./diff-cam-engine";
-import dynabyteLogo from "./dynabyte_white.png";
-import { ICapturePayload, IDiffCamEngine } from "./models/diffCamEngine.models";
-import Logo from "./shared/Logo";
-import Title from "./shared/Title";
+import React from 'react';
+import './App.css';
+import { DiffCamEngine } from './diff-cam-engine';
+import dynabyteLogo from './dynabyte_white.png';
+import { ICapturePayload, IDiffCamEngine } from './models/diffCamEngine.models';
+import Logo from './shared/Logo';
+import Title from './shared/Title';
 
 export const App = () => {
   // const [dataUrl, setDataUrl] = React.useState<string>("");
   const [hasMotion, setHasMotion] = React.useState<boolean>(false);
 
+  const videoElement: HTMLVideoElement = document.createElement('video');
+  const canvasElement: HTMLCanvasElement = document.createElement('canvas');
+
   const diffCamEngine: IDiffCamEngine = DiffCamEngine();
+
   const initSuccess: () => void = () => {
     diffCamEngine.start();
   };
@@ -23,36 +27,37 @@ export const App = () => {
     payload: ICapturePayload
   ) => {
     setHasMotion(payload.hasMotion);
-    console.log(payload.getURL());
+    if (payload.hasMotion) {
+      console.log(payload.getURL());
+    }
   };
-  const videoElement: HTMLVideoElement = document.createElement("video");
-  const canvasElement: HTMLCanvasElement = document.createElement("canvas");
+
   diffCamEngine.init({
     video: videoElement,
     motionCanvas: canvasElement,
     initSuccessCallback: initSuccess,
     initErrorCallback: initError,
     captureCallback: capture,
-    captureIntervalTime: 2000,
+    captureIntervalTime: 5000,
   });
   return (
-    <div className="wrapper">
+    <div className='wrapper'>
       <Title hasMotion={hasMotion}>Välkommen till</Title>
       <Logo
         src={dynabyteLogo}
-        alt="logo"
-        width="200"
-        height="80"
+        alt='logo'
+        width='200'
+        height='80'
         hasMotion={hasMotion}
       />
       <footer>
         <span>
-          Photo by{" "}
-          <a href="https://unsplash.com/@freetousesoundscom?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">
+          Photo by{' '}
+          <a href='https://unsplash.com/@freetousesoundscom?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText'>
             Free To Use Sounds
-          </a>{" "}
-          on{" "}
-          <a href="https://unsplash.com/s/photos/grass?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">
+          </a>{' '}
+          on{' '}
+          <a href='https://unsplash.com/s/photos/grass?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText'>
             Unsplash
           </a>
         </span>
--- src/shared/Logo.tsx
+++ src/shared/Logo.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import '../App.css';
 
 interface ILogo
   extends React.DetailedHTMLProps<
