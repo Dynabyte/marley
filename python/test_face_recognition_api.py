dcac9965914c8e7198e145b6f3e37d8b47dab876
daniel-hughes-nackademin <daniel.hughes.sweden@gmail.com>
2020-09-30 16:21:55 +0200

    added test for predict and generated test data

---
+++ python/test_face_recognition_api.py
@@ -0,0 +1,16 @@
+import face_recognition
+import face_recognition_api
+import json
+import os
+
+json_file_name = 'encoding_data.json'
+
+def test_predict():
+    image = face_recognition.load_image_file('test_images/person1.jpg')
+    encoding = face_recognition \
+        .face_encodings(image)[0]
+    with open(json_file_name) as json_file:
+        faces = json.load(json_file)
+    
+    assert face_recognition_api.predict(encoding, faces) == '1'
+
