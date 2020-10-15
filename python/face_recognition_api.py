2e39ebe517c1ccdde5808c54a094bf04833f222e
Daniel Hughes <daniel.hughes.sweden@gmail.com>
2020-10-15 17:44:53 +0200

    working Dockerfile, changed flask server to waitress

--- python/face_recognition_api.py
+++ python/face_recognition_api.py
@@ -2,6 +2,7 @@ import traceback
 from collections import namedtuple
 import numpy as np
 import flask as fl
+from waitress import serve
 from flask_restx import Resource, Api, fields
 import io
 import base64
@@ -252,4 +253,5 @@ def print_time(name, start):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1')
+    # app.run(debug=True, host='0.0.0.0')
+    serve(app, host='0.0.0.0', port=5000)
