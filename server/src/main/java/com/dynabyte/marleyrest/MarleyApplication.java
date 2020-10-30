5e6ebf1372d28445d8f48b451fead345ba5f3302
Daniel Hughes <daniel.hughes.sweden@gmail.com>
2020-10-30 13:36:57 +0100

    Swagger UI URL settings and minor testing modifications

--- server/src/main/java/com/dynabyte/marleyrest/MarleyApplication.java
+++ server/src/main/java/com/dynabyte/marleyrest/MarleyApplication.java
@@ -2,6 +2,14 @@ package com.dynabyte.marleyrest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+import java.net.URI;
+
+import static org.springframework.web.servlet.function.RequestPredicates.GET;
+import static org.springframework.web.servlet.function.RouterFunctions.route;
 
 /**
  * Runs the rest api spring boot application
@@ -11,6 +19,12 @@ public class MarleyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MarleyApplication.class, args);
+    }
+
+    @Bean
+    RouterFunction<ServerResponse> routerFunction() {
+        return route(GET("/swagger"), req ->
+                ServerResponse.temporaryRedirect(URI.create("/documentation/swagger-ui/index.html#/marley-rest-controller")).build());
     }
 
 }
